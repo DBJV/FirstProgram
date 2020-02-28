9660ddd1530cf39906f8ab7bccec1a5ba1997613
DBJV <dawid.bednarek94@gmail.com>
2020-02-28 17:39:04 +0100

    Create my first class!

--- src/main/java/pl/learning/Main.java
+++ src/main/java/pl/learning/Main.java
@@ -1,8 +1,15 @@
 package pl.learning;
 
+import pl.learning.component.GreetingComponent;
+
+import java.time.LocalDateTime;
+
 public class Main {
     public static void main (String []args){
+        GreetingComponent greeting = new GreetingComponent();
+        greeting.sayHello();
+        greeting.sayHello();
+        greeting.sayHello();
 
-        System.out.println("Hello World");
     }
 }
---
+++ src/main/java/pl/learning/component/GreetingComponent.java
@@ -0,0 +1,16 @@
+package pl.learning.component;
+
+import java.time.LocalDateTime;
+
+public class GreetingComponent {
+
+    LocalDateTime creationDate;
+
+   public GreetingComponent (){
+        creationDate = LocalDateTime.now();
+    }
+
+    public void sayHello() {
+        System.out.println("Hello World. I was created at : " + creationDate);
+    }
+}
